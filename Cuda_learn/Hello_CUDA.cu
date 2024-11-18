3ecee19818215a5b0f428323841d3b5cb43f2023
Sakai04 <pxj0684@gmail.com>
2024-11-18 13:50:48 +0900

    초기 커밋.

---
+++ Cuda_learn/Hello_CUDA.cu
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+__global__ void helloCuda(void)
+{
+    printf("hello cuda from gpu\n");
+}
+
+int main(void)
+{
+    printf("hello gpu from cpu!\n");
+    helloCuda << <1, 10 >> > ();
+    cudaDeviceSynchronize(); // GPU 작업이 완료될 때까지 기다림
+    return 0;
+}
